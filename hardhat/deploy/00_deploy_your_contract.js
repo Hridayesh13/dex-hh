824d16ffc77c99812a0db4ae56abb9a183f0ef9a
Hridayesh Akhand <hridayesh@Hridayeshs-MacBook-Air.local>
2021-07-08 12:06:21 +0530

    added react-app & fixed deploy & publish.js

--- hardhat/deploy/00_deploy_your_contract.js
+++ hardhat/deploy/00_deploy_your_contract.js
@@ -1,12 +1,21 @@
 // deploy/00_deploy_your_contract.js
+const fs = require('fs');
+const chalk = require('chalk');
+const ethers = require('ethers');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("Balloons", {
+  const balloons = await deploy("Balloons", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     // args: ["Hello"],
+    log: true,
+  });
+  const dex = await deploy("DEX", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args: [balloons.address],
     log: true,
   });
 
@@ -18,7 +27,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     //const Balloons = await ethers.getContractAt('Balloons', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   */
 };
-module.exports.tags = ["Balloons"];
+module.exports.tags = ["Balloons", "DEX"];
 
 /*
 Tenderly verification
